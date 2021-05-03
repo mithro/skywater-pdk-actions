6d6a8d14016403986b34f888638545c5a75a7b5f
Tim 'mithro' Ansell <tansell@google.com>
2021-05-02 17:14:46 -0700

    Pending token.

--- modules-pr-backporter/library_submodules.py
+++ modules-pr-backporter/library_submodules.py
@@ -81,17 +81,17 @@ def git_config(gitdir, key, value, level='local'):
     git("config --{} '{}' '{}'".format(level, key, value), gitdir)
 
 
-#def github_auth_set(gitdir, access_token):
-#    auth_token = base64.b64encode('x-access-token:{}'.format(access_token).encode('utf-8'))
-#    extra_header = "AUTHORIZATION: basic {}".format(auth_token.decode())
-#    git_config(gitdir, 'http.https://github.com/.extraheader', extra_header)
+def github_auth_set(gitdir, access_token):
+    auth_token = base64.b64encode('x-access-token:{}'.format(access_token).encode('utf-8'))
+    extra_header = "AUTHORIZATION: basic {}".format(auth_token.decode())
+    git_config(gitdir, 'http.https://github.com/.extraheader', extra_header)
 
 
-def github_auth_set(gitdir):
-    # git config core.sshCommand "ssh -i ~/.ssh/id_rsa_example -F /dev/null"
-    ssh_keys.import_keys()
-    #git_config(gitdir, "protocol.allow", "never", level='system')
-    #git_config(gitdir, "protocol.ssh.allow", "always", level='system')
+#def github_auth_set(gitdir):
+#    # git config core.sshCommand "ssh -i ~/.ssh/id_rsa_example -F /dev/null"
+#    ssh_keys.import_keys()
+#    #git_config(gitdir, "protocol.allow", "never", level='system')
+#    #git_config(gitdir, "protocol.ssh.allow", "always", level='system')
 
 
 def out_v(v, versions):
